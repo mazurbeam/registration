c52e4bb0d2946df374e25ee87243721a0876d59b
mazurbeam <mazurbeam@gmail.com>
2017-08-11 11:38:23 -0700

    added form to html template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/registration/user.ts
@@ -0,0 +1,13 @@
+export class User {
+  constructor(
+    public firstName: string = "",
+    public lastName: string = "",
+    public email: string = "",
+    public password: string = "",
+    public address: string = "",
+    public unit: string = "",
+    public city: string = "",
+    public state: string = "",
+    public lucky: string = ""
+  ){}
+}
